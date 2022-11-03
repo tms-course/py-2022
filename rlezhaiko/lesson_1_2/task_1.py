e3c6cd6d13c1a6750e3889993aa09306f4bd8695
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-03 20:01:19 +0300

    lesson_1_2

---
+++ rlezhaiko/lesson_1_2/task_1.py
@@ -0,0 +1,9 @@
+a = float(input('Введите первое действительное число: '))
+b = float(input('Введите второе действительное число: '))
+
+print('a + b =', a+b)
+print('a - b =', a-b)
+print('a * b =',a*b)
+print('a ** b =',a**b)
+print("a % b =",a%b)
+print('a // b =',a//b)
