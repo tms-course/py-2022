d27a055c1c9b3bae18f4cd519023a2fb292bf33c
VasilionakA <vasilenok_nastya@mail.ru>
2022-11-11 16:29:34 +0200

    created folder lesson_3

---
+++ vasilionak/lesson_3/task_4.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = 0
+for i in range(n = int(input())
+a = 0
+for i in range(1, n+1):
+    a += i**3
+print(a)
+
+n = int(input())
+i = 1
+a = 0
+while i <= n:
+    a += i**3
+    i += 1
+print(a)
