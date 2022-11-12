4b9eeaa60d3a2b4432b09da5c8874a5c50cc7d17
Yegor Pikulik <egorka8595@mail.ru>
2022-11-12 21:43:11 +0300

    edit task_1-5.py

--- Yegor/lesson_3/task_1-5.py
+++ Yegor/lesson_3/task_1-5.py
@@ -6,21 +6,33 @@ print('!{0} ! {1}!'.format(words[1], words[0]))
 
 # 2-3 task
 while True:
-    name = input('Введите свое имя: ')
-    age = input('Введите свой возраст: ')
-    if age == 'exit':
-        break
-    elif not age.isdigit() or int(age) <= 0:
-        print('Ошибка, повторите ввод')
-    elif int(age) > 0 and int(age) < 10:
-        print('Привет, шкет', name)
-    elif int(age) >= 10 and int(age) <=18:
-        print('Как жизнь, ', name)
-    elif int(age) > 18 and int(age) < 100:
-        print('Что желаете, ', name, '?')
+     name = input('Введите свое имя: ')
+     age = input('Введите свой возраст: ')
+     if not age.isdigit() or int(age) <= 0:
+         print('Ошибка, повторите ввод')
+     age = int(age)
+     if age > 0 and age < 10:
+         print('Привет, шкет', name)
+     elif age >= 10 and age <= 18:
+         print('Как жизнь, ', name)
+     elif age > 18 and age < 100:
+         print('Что желаете, ', name, '?')
+     elif age > 100:
+         print(name, 'вы лжете - в наше время столько не живут...')
 
-    elif int(age) > 100:
-        print(name, 'вы лжете - в наше время столько не живут...')
+
+# 5 task
+import random
+random_number = random.randint(0, 10)
+while True:
+    number = int(input('Угадайте число:'))
+    if number == random_number:
+        print('тебе улыбнулась удача')
+        break
+    if number > random_number:
+        print('выбери число меньше')
+    elif number < random_number:
+        print('выбери число больше')
 
 
 # 4task
@@ -37,16 +49,3 @@ while x <= n:
     result += x**3
     x += 1
 print(result)
-
-# 5 task
-import random
-n = random.randint(0, 10)
-while True:
-    number = int(input('Угадайте число:'))
-    if number == n:
-        print('тебе улыбнулась удача')
-        break
-    if number > n:
-        print('выбери число меньше')
-    elif number < n:
-        print('выбери число больше')
