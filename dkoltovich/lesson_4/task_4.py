4a591f91bcaa2d4ee624d0e912aad8bc170668c6
Denis Koltovich <koltovich1123@gmail.com>
2022-11-12 17:17:49 +0300

    fix lesson 4

--- dkoltovich/lesson_4/task_4.py
+++ dkoltovich/lesson_4/task_4.py
@@ -2,15 +2,11 @@ import time
 from datetime import datetime
 
 
-def time_list_generator(n: int) -> list:
-    tmp_list = []
-    for i in range(n):
-        tmp_list.append(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'))
-        time.sleep(1)
-
-    return tmp_list
+def time_list_generator() -> str:
+    time.sleep(1)
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
 
 number_of_elements = int(input('Введите кол-во элементов списка: '))
-time_list = [i for i in time_list_generator(number_of_elements)]
+time_list = [time_list_generator() for i in range(number_of_elements)]
 print(time_list)
--- dkoltovich/lesson_4/words_ranker.py
+++ dkoltovich/lesson_4/words_ranker.py
@@ -1,32 +1,26 @@
 import sys
+from collections import defaultdict
 
 input_text = '''Что тебе, что мне, что ей - всем нужно решить эту задачу,
 а что потом будет, потом и увидим, а увидим мы непременно.'''
 
 
-def parse_arguments(line_of_arguments: str):
-    n_index = line_of_arguments.find('-n')
-    k_index = line_of_arguments.find('-k')
-    if n_index > k_index:
-        n_index, k_index = k_index, n_index
-        k, n = line_of_arguments[n_index + 2: k_index], line_of_arguments[k_index + 2::]
-    else:
-        n, k = line_of_arguments[n_index + 2: k_index], line_of_arguments[k_index + 2::]
-
+def get_arguments():
+    [n, k] = [sys.argv[2], sys.argv[4]] if sys.argv[1] == '-n' else [sys.argv[4], sys.argv[2]]
     return int(n), int(k)
 
 
 def swap_keys_with_values(dictionary: dict) -> dict:
-    swap_dict = {}
+    swap_dict = defaultdict(list)
     for key, value in dictionary.items():
-        swap_dict[value] = swap_dict.get(value, []) + [key]
+        swap_dict[value].append(key)
     return swap_dict
 
 
-def counter(list_of_items: list) -> dict:
-    amount_of_elements = {i: 0 for i in list_of_items}
+def count_elements(list_of_items: list) -> dict:
+    amount_of_elements = {}
     for item in list_of_items:
-        amount_of_elements[item] = amount_of_elements.get(item) + 1
+        amount_of_elements[item] = amount_of_elements.get(item, 0) + 1
 
     '''возращает словарь, где ключи - слова, а значения - кол-во раз, которое они встретились в тексте'''
     return amount_of_elements
@@ -37,17 +31,16 @@ def create_list_of_words(text: str) -> list:
     return [word.lower() for word in text.split() if word.isalpha()]
 
 
-def count_words_amount(list_of_words: list) -> dict:
-    dictionary = counter(list_of_words)  # словарь, где ключи - слова, а значения - сколько раз она встретились
-    dictionary = swap_keys_with_values(dictionary)
+def create_counter_dict(list_of_words: list) -> dict:
+    counter_dict = count_elements(list_of_words)  # словарь, где ключи - слова, а значения - сколько раз она встретились
+    counter_dict = swap_keys_with_values(counter_dict)
     '''возращает отсортированный по убыванию словарь, где значения - слова, 
        а ключи - сколько раз они встретились в тексте'''
-    return dict(sorted(dictionary.items(), reverse=True))
+    return dict(sorted(counter_dict.items(), reverse=True))
 
 
-amount_dictionary = count_words_amount(create_list_of_words(input_text))
-arguments_line = ''.join(sys.argv)
-n_argument, k_argument = parse_arguments(arguments_line)
+amount_dictionary = create_counter_dict(create_list_of_words(input_text))
+n_argument, k_argument = get_arguments()
 for key in amount_dictionary.keys():
     if n_argument == 0:
         break
