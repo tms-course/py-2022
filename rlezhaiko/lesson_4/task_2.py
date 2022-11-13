66dc0d0a5993ddedbd4f6f263244710642546ee8
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-13 21:56:24 +0300

    Change name of function in task_2.py

--- rlezhaiko/lesson_4/task_2.py
+++ rlezhaiko/lesson_4/task_2.py
@@ -1,8 +1,8 @@
-def recursion(n: int) -> int:
+def get_factorial(n: int) -> int:
     if n == 1:
         return 1
-    return n * recursion(n-1)
+    return n * get_factorial(n-1)
 
 
 n = int(input('Enter n: '))
-print(recursion(n))
+print(get_factorial(n))
