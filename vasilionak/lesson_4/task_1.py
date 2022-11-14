1bef81401409d5200b199847b3ea1fd0ed0de608
VasilionakA <vasilenok_nastya@mail.ru>
2022-11-14 12:04:40 +0200

    fix task_1-3

--- vasilionak/lesson_4/task_1.py
+++
@@ -1,8 +0,0 @@
-def my_func(**kwargs):
-    keys, values = [], []
-    for key, value in kwargs.items():
-        print(value, key)
-        keys.append(key)
-        values.append(value)
-    return values, keys
-keys, values = my_func(a=5, b=7, c=9)
--- vasilionak/lesson_4/task_2.py
+++
@@ -1,9 +0,0 @@
-number = int(input())
-def factorial_recursive(number):
-    if number == 1:
-        return number
-    else:
-        return number*factorial_recursive(number-1)
-
-print(factorial_recursive(number))
-
