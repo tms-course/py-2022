6454fcd0bcaca023339be2bb7744c6500b92a836
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-15 21:01:33 +0300

    Add module documentation and fix some mistakes in task_5.py

--- rlezhaiko/lesson_5/task_5.py
+++ rlezhaiko/lesson_5/task_5.py
@@ -1,4 +1,14 @@
-def check_string(string: str):
+"""
+5. **Сделать функцию которая на вход принимает строку. Анализирует ее
+исключительно методом isdigit() без доп. библиотек и переводит строку в число.
+Функция умеет распознавать отрицательные число и десятичные дроби. Примеры:
+-6.7     ->   Вы ввели отрицательное дробное число: -6.7
+5        ->   Вы ввели положительное целое число: 5
+5.4r     ->   Вы ввели не корректное число: 5.4r
+-.777    ->   Вы ввели отрицательно дробное число: -0.777
+"""
+
+def check_string_for_valid_number(string: str):
     """
     Check string function
     
@@ -14,11 +24,11 @@ def check_string(string: str):
         elif flag_negative_number and not flag_fractional_number:
             print(f'Вы ввели отрицательное целое число {string}')
         elif not flag_negative_number and flag_fractional_number:
-            print(f'Вы ввели положительное дробное число {str(float(string))}')
+            print(f'Вы ввели положительное дробное число {float(string)}')
         else:
-            print(f'Вы ввели отрицательное дробное число {str(float(string))}')
+            print(f'Вы ввели отрицательное дробное число {float(string)}')
     else:
         print(f'Вы ввели не корректное число {string}')  
 
 
-check_string('-.0')
+check_string_for_valid_number('-.0')
