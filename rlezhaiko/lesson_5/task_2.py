7cc0bb9754fcf6c6b432f04fddad125165878107
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-15 20:28:14 +0300

    Add module documentation in task_2.py

--- rlezhaiko/lesson_5/task_2.py
+++ rlezhaiko/lesson_5/task_2.py
@@ -1,3 +1,8 @@
+"""
+2. Дан список чисел. Вернуть список, где при помощи функции map() каждое число
+переведено в строку. В качестве функции в map использовать lambda 
+"""
+
 list_of_numbers = [i for i in range(20)]
 list_of_numbers_in_str_format = list(map(lambda x: str(x), list_of_numbers))
 print(list_of_numbers_in_str_format)
