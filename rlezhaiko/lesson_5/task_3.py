21fcab84fcdd1103e7fee4ff87d5d354c6189dae
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-15 18:35:23 +0300

    Rewrite function in task_3.py

--- rlezhaiko/lesson_5/task_3.py
+++ rlezhaiko/lesson_5/task_3.py
@@ -1,10 +1,12 @@
 def check_palindrome(string:str) -> bool:
+    """
+    Check palindrom function.
+    
+    :param word: the word to check for a palindrom
+    :returns: return True if the word is a palindrom, False otherwise
+    """
     string = string.lower()
-    reversed_string = string[::-1]
-    if string == reversed_string:
-        return True
-    else:
-        return False
+    return string == string[::-1]
 
 
 tuple_of_words = ('Radar', 'apple', 'Madam', 'Tree', 'some', 'level', 'nun')
