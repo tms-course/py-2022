8fc2cdb40b4236e60ae1ead78f5537725508b65e
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-17 00:24:20 +0300

    Start writing task_3.py

--- rlezhaiko/lesson_6/task_3.py
+++ rlezhaiko/lesson_6/task_3.py
@@ -3,3 +3,16 @@
 значений кортеж состоящий из 2-х элементов - имя(str), возраст(int). Сделать около 5-6 
 элементов словаря. Записать данный словарь на диск в json-файл.
 """
+
+from random import randint
+keys = [randint(100000, 999999) for _ in range(6)]
+print(keys)
+names = ['John', 'Silva', 'Sam', 'Kate', 'Rick', 'Morty']
+values = [(names[i], randint(1, 100)) for i in range(6)]
+print(values)
+dict_users = {}.fromkeys(keys, ())
+i = 0
+for key, _ in dict_users.items():
+    dict_users[key] = values[i]
+    i += 1
+print(dict_users)
