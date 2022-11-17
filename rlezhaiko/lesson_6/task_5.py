b042a0e6e164bbf92ec2d7ee93d839e4916cfaae
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-17 23:03:56 +0300

    Add task_5.py

--- rlezhaiko/lesson_6/task_5.py
+++ rlezhaiko/lesson_6/task_5.py
@@ -11,3 +11,25 @@ name   |   Sam    |   Adam   |   Jasy
 phone  | 097-32-88| 090-45-41| 094-66-22
 ----------------------------------------
 """
+
+import csv
+import openpyxl
+
+rows = []
+with open('task_4_data.csv', 'r') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    for row in csv_reader:
+        rows.append(row)
+
+workbook = openpyxl.Workbook()
+active_sheet = workbook.active
+active_sheet.append(['' if i == 0 else f'Person {i}' for i in range(len(rows))])
+for i in range(len(rows[0])):
+    if i != 2:
+        list_tmp = []
+        for element in rows:
+            list_tmp.append(element[i])
+        active_sheet.append(list_tmp)
+
+workbook.save('task_5_data.xlsx')
+workbook.close()
