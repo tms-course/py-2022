746cbf4fe4367860f107fe0d0296c86af7d7621f
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-17 23:05:07 +0300

    Start writing extra.py

--- rlezhaiko/lesson_6/extra.py
+++ rlezhaiko/lesson_6/extra.py
@@ -4,3 +4,31 @@
 (даже вложенному), удалять значение, показывать состояние коллекции. Выбор структуры 
 данных для хранения на ваше усмотрение.
 """
+
+from sys import stdin
+from string import punctuation
+
+help_str = '''
+!help - Выводит описание функций
+!exit - Выход из программы'''
+print('Введите !help для просмотра функций.')
+
+
+def get_and_check_commands(string: str) -> list:
+    words = string.replace('!', '', 1).split()
+    if len(words) == 2:
+        return list(words[0], words[1], True)
+    else:
+        return list(None, None, False)
+
+
+for line in stdin:
+    line = line.strip()
+    if line[0] == '!':
+        command, argument, valid = get_and_check_commands(line)
+        if valid:
+            pass
+        
+    elif line[0] in punctuation.replace('!', ''):
+        print('Такой комманды не существует. Введите !help для просмотра функций.')
+    print(line)
