996e7b5fbbbca96b29ae91cc6eee68f11c69a7c8
VasilionakA <91758346+VasilionakA@users.noreply.github.com>
2022-11-20 22:15:03 +0200

    Update task_1.py
    fixed task_1

--- vasilionak/lesson_5/task_1.py
+++ vasilionak/lesson_5/task_1.py
@@ -1,8 +1,10 @@
-x = int(input("Введите число: "))
-answer = (lambda x: "четное" if x % 2 == 0 else "нечетное")(x)
 """
 принимает число и выдает четное оно или нет
 param x: number for check 
 param answer: return result
 """
+
+x = int(input("Введите число: "))
+answer = (lambda x: "четное" if x % 2 == 0 else "нечетное")(x)
+
 print(answer)
