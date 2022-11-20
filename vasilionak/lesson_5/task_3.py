5f4d605e678c28e79dd761dc5f3f87598664dae6
VasilionakA <91758346+VasilionakA@users.noreply.github.com>
2022-11-20 22:16:46 +0200

    Update task_3.py
    fixed task_3

--- vasilionak/lesson_5/task_3.py
+++ vasilionak/lesson_5/task_3.py
@@ -1,6 +1,7 @@
-words = ('wow', 'world', 'mom', 'cat', 'eye')
-filtered_list = (list(filter(lambda item: item == item[::-1], words)))
-print(filtered_list)
 """
 отфильтровывает слова и выдает только те что являются полиндромами
 """
+
+words = ('wow', 'world', 'mom', 'cat', 'eye')
+filtered_list = (list(filter(lambda item: item == item[::-1], words)))
+print(filtered_list)
