5be930cd154233e9de186c84c6bf4472d72533ed
Maksim Shik <mshik@comscore.com>
2022-11-20 19:09:09 +0300

    removed whitespace

--- mshik/lesson_5/task3.py
+++ mshik/lesson_5/task3.py
@@ -4,6 +4,5 @@
 При помощи filter() из кортежа слов отфильтровать толко те,
 которые являются полиндромами (которые читаются одинаково в обе стороны).
 """
-
 polindroms = list(filter(lambda word: word == word[::-1], ["hello", "abba", "test", "anna"]))
 print(f"Polindroms: {polindroms}")
