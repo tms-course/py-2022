d596168dd38cfb1fc5587c0011c59b1a03ac37b0
Alexey <kotlov_alexey@mail.ru>
2022-11-21 15:11:27 +0300

    verification

---
+++ Kotlov/lesson_1_2/task_1.py
@@ -0,0 +1,5 @@
+a = float(input('Введите a: '))
+b = float(input('Введите b: '))
+
+print(f"""Sum = {a + b}\nDifference = {a - b}\nProd = {a * b}
+Degree = {a ** b}\nDiv % = {a % b}\nDiv // = {a // b}""")
