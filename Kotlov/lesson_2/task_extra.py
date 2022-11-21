e2689cc51fec8d1fed97ea760de16b35166c2bf6
Alexey <kotlov_alexey@mail.ru>
2022-11-21 16:47:15 +0300

    Removed unnecessary files and renamed others

--- Kotlov/lesson_2/task_extra.py
+++
@@ -1,9 +0,0 @@
-import keyword
-
-a = input('Как вы хотите назвать переменную?\n')
-b = list(a)
-c = a.replace('_', 'a')
-bool_value = not (' ' in b or b[0].isdigit() or keyword.iskeyword(a) or not c.isalnum())
-
-print(f'Is variable name "{a}" valid ? - {bool_value}')
-
