25bd61c43a38b804202e4eda2431001a27f054a9
Denis Koltovich <koltovich1123@gmail.com>
2022-11-22 22:19:05 +0300

    added docstrings for all  modules, classes, methods

--- dkoltovich/lesson_8/task_1-2.py
+++ dkoltovich/lesson_8/task_1-2.py
@@ -1,8 +1,30 @@
+"""
+task1-2:
+Создать родительский класс Auto с методами move, stop birthday, и унаследовать от него классы Truck, Car.
+Переопределить методы. В класс Truck добавить метод load
+"""
 import time
 
 
 class Auto:
+    """
+    Base class auto
+    Attributes:
+        brand: str
+        age: int
+        mark: str
+        color: str = 'black'
+        weight: int = 500
+    """
     def __init__(self, brand: str, age: int, mark: str, color: str = 'black', weight: int = 500):
+        """
+        Initialisation of Auto object
+        :param brand: Auto's brand
+        :param age: Auto's age
+        :param mark: Auto's mark
+        :param color: Auto's color, black by default
+        :param weight: Auto's weight, 500 by default
+        """
         self.brand = brand
         self.age = age
         self.color = color
@@ -10,40 +32,107 @@ class Auto:
         self.weight = weight
 
     def move(self):
+        """
+        prints 'move'
+        :return: None
+        """
         print("move")
 
     def stop(self):
+        """
+        prints 'stop'
+        :return: None
+        """
         print("stop")
 
     def birthday(self):
+        """
+        Increases age on 1
+        :return: None
+        """
         self.age += 1
 
     def get_age(self):
+        """
+        Getter method
+        :return: Auto's age
+        """
         return self.age
 
 
 class Truck(Auto):
+    """
+       Inherited class Truck
+       Attributes:
+           brand: str
+           age: int
+           mark: str
+           color: str = 'black'
+           weight: int = 500
+           max_load: int
+       """
 
     def __init__(self, brand: str, age: int, mark: str, max_load: int, color='black', weigth=500):
+        """
+        Init Truck object
+        :param brand:
+        :param age:
+        :param mark:
+        :param max_load:
+        :param color:
+        :param weigth:
+        """
         super().__init__(brand, age, mark, color, weigth)
         self.max_load = max_load
 
     def move(self):
+        """
+        Prints 'attention', then prints 'move'
+        :return: None
+        """
         print('Attention')
         super().move()
 
     def load(self):
+        """
+        Waits for a sec, then prints 'load' and waits for a second again
+        :return: None
+        """
         time.sleep(1)
         print('load')
         time.sleep(1)
 
 
 class Car(Auto):
+    """
+    Inherited class Truck
+       Attributes:
+           brand: str
+           age: int
+           mark: str
+           color: str = 'black'
+           weight: int = 500
+           max_speed: int
+    """
+
     def __init__(self, brand: str, age: int, mark: str, max_speed: int, color='black', weigth=500):
+        """
+        Init Car object
+        :param brand:
+        :param age:
+        :param mark:
+        :param max_speed:
+        :param color:
+        :param weigth:
+        """
         super().__init__(brand, age, mark, color, weigth)
         self.max_speed = max_speed
 
     def move(self):
+        """
+        Prints 'move' then prints max_speed
+        :return: None
+        """
         super().move()
         print(f'Max speed is {self.max_speed}')
 
--- dkoltovich/lesson_8/task_3.py
+++ dkoltovich/lesson_8/task_3.py
@@ -1,9 +1,27 @@
+"""
+task 3:
+Реализовать для класса Circle, рассмотренного на уроке, метод вычитания окружностей.
+Если радиусы окружностей равны, то вернуть объект класса Point
+
+"""
+
 import math
 from math import sqrt
 
 
 class Point:
+    """
+    Class that represents a Point
+    Attributes:
+        x: float
+        y: float
+    """
     def __init__(self, x: float, y: float):
+        """
+        Initialisation of Point object
+        :param x: coordinate x
+        :param y: coordinate y
+        """
         self.x = x
         self.y = y
 
@@ -15,25 +33,47 @@ class Point:
         return sqrt(self.x ** 2 + self.y ** 2)
 
     def __eq__(self, other):
+        """
+        comparison of Points
+        :param other: other Point to compare
+        :return: True if points are equal, False otherwise
+        """
         return other.x == self.x and other.y == self.y
 
 
 class Circle(Point):
+    """
+    Class that represents a circle
+    Attributes:
+        x: float
+        y: float
+        radius: float
+    """
     def __init__(self, radius: int):
+        """Initialisation of Circle object
+        :param radius: radius of the circle
+        """
         super().__init__(radius, 0)
         self.radius = radius
 
     def edge_distance_from_origin(self):
+        """
+        :return: Circle radius
+        """
         return self.radius
 
     def area(self):
+        """
+        Calculates the aria of the circle
+        :return: area
+        """
         return math.pi * self.radius ** 2
 
     def __sub__(self, other):
         """
-        Subdivision of two Circles
+        Subtracting of two Circles
         :param other: other Circle
-        :return: origin point in case of equivalence of radiuses, else Circle with radius as subdivision result
+        :return: origin point in case of equivalence of radiuses, else Circle with radius as subtracting result
         """
         if self.radius == other.radius:
             return Point(0, 0)
@@ -48,5 +88,10 @@ class Circle(Point):
         return 2 * math.pi * self.radius
 
     def __eq__(self, other):
+        """
+        comparison of two circles
+       :param other:  Circle to compare
+       :return: True if circles are equal, False otherwise
+        """
         return self.radius == other.radius
 
