9976a07ec6e4c10adaadcefd2748ff4d9f73fc59
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-22 18:36:33 +0300

    Rewrite task_4.py

--- rlezhaiko/lesson_6/task_4.py
+++ rlezhaiko/lesson_6/task_4.py
@@ -7,16 +7,13 @@ from random import randint
 import json
 import csv
 
-list_of_users = []
+list_of_users, rows = [], [['id', 'name', 'age', 'phone']]
 with open('task_3_data.json', 'r') as f:
     list_of_users = json.load(f)
 
-rows = [['id', 'name', 'age', 'phone']]
-phone_numbers = [str(randint(100, 999)) + '-' + str(randint(0, 99)) + '-' + str(randint(0, 99)) for _ in range(len(list_of_users))]
+gen_phone_num = lambda: str(randint(100, 999)) + '-' + str(randint(0, 99)) + '-' + str(randint(0, 99))
 for user in list_of_users:
-    list_tmp = list(user.values())
-    list_tmp.append(phone_numbers[list_of_users.index(user)])
-    rows.append(list_tmp)
+    rows.append([*user.values(), gen_phone_num()])
 
 with open('task_4_data.csv', 'w', newline='') as f:
     csv_writer = csv.writer(f, delimiter=',')
