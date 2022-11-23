9a8a84260a610763d034bc6f1694bfbfec809ee9
Alexey <kotlov_alexey@mail.ru>
2022-11-23 15:51:20 +0300

    Fix

--- Kotlov/lesson_4/extra.py
+++ Kotlov/lesson_4/extra.py
@@ -15,46 +15,25 @@ for i in our_text:  # Удаляем все не нужные элементы
 
 our_text = our_text.split()  # Делаем список из элементов(слов) строки our_text
 
-word_count = {i: our_text.count(i) for i in our_text}  # Создаём словарь элементами которого являются слова и их количество в тексте
+word_count = {}  # Создаём словарь элементами которого являются слова и их количество в тексте
+for i in our_text:
+    word_count[i] = word_count.get(i, 0) + 1
 
+word_count_2 = {}
 
-def words_ranker(text_num: dict, count_n=0) -> str:
-    """
-    Печатает n строк встречающихся слов по порядку убывания кол-ва повторений этого слова в тексте.
-    При одинаковом кол-ве вхождений слов, выводим первые k из них.
-    """
+for key, value in word_count.items():
+    if value in word_count_2:
+        word_count_2[value] = word_count_2.get(value) + [key]
+    else:
+        word_count_2[value] = [key]
 
-    list_equal_count, list_equal_count_k = [], []   #  Cоздаём список со словами с одинаковым кол-во повторения
-                                                    #  и список со словами с одинаковым кол-во повторения
-                                                    #  + количество,которых не превосходит k
+count_n = 0
 
-    maxi_n = 0  # Кол-во повторений слова в тексте
-
-    if count_n == n:  # Проверка кол-ва выведенных строк с тем которое задали мы в начале(эта проверка стоит в начале,а
-                      # не в конце,т.к, если мы введём n = 0,то всё полетит))
-        return ''
-
-    for key, value in text_num.items():  # Находим кол-во, самого часто встречаемого элемента
-        if value > maxi_n:
-            maxi_n = value
-
-    for key, value in text_num.items():  # Находим слова,которые встречаюся maxi_n раз + проверяем сколько максимум таких слов может быть в строке
-        if value == maxi_n:
-            list_equal_count.append(key)
-
-            if len(list_equal_count_k) < k:
-                list_equal_count_k.append(key)
-
-    print(maxi_n, end=' ')                  # Выводим наши строки, сначала кол-во повторений этих слов в тексте,а потом сами слова
-    print(*list_equal_count_k, sep=', ')
-
-    count_n += 1  # Cчитаем кол-во строк (уже выведенных)
-
-    text_num = {key: value for key, value in text_num.items() if key not in list_equal_count}  # Убираем уже использованные элементы
-
-    return None if text_num == {} else words_ranker(text_num, count_n)  # Если все слова выведены,то выходим из функции,иначе
-                                                                # выполняем words_ranker() заново, до тех пор,
-                                                                # пока у нас не останется не выведенных слов
-
-
-words_ranker(word_count)
+while count_n < n:
+    max_key = max(word_count_2)
+    print(max_key, end=' ')
+    print(*word_count_2[max_key][:k], sep=', ')
+    count_n += 1
+    del word_count_2[max_key]
+    if word_count_2 == {}:
+        break
--- Kotlov/lesson_4/task_3.py
+++ Kotlov/lesson_4/task_3.py
@@ -1,16 +1,17 @@
 from random import randint
 
-list_1 = [randint(1, 10) for _ in range(11)]
+list_1 = [randint(1, 10) for _ in range(100000)]
 
 
 def dublicate(lst: list) -> dict:
     """
     Функция считает сколько каких цифр в списке
     """
-    dict_1 = {key: lst.count(key) for key in lst}
+    dict_1 = {}
+    for i in lst:
+        dict_1[i] = dict_1.get(i, 0) + 1
+
     return dict_1
 
-
-print(list_1)
 
 print(dublicate(list_1))
