5916c5ed9aa47ec62b0a675371ba1df92369e6b2
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-11-25 18:34:57 +0300

    Fix task_2, task_3

--- rlezhaiko/lesson_9/task_2.py
+++ rlezhaiko/lesson_9/task_2.py
@@ -19,35 +19,64 @@ class User(object):
         self.__password = password
     
 
-    def get_user(self) -> str:
+    def get_email(self) -> str:
         """ 
-        Getter function
+        Get email function
         
         :param self: self object of class
         :returns: return str
         """
-        print('Getter work...')
-        return f'{self.__email} {self.__password}'
+        print('Getter email work...')
+        return f'{self.__email}'
     
     
-    def set_user(self, email, password) -> None:
+    def set_email(self, email) -> None:
         """ 
-        Setter function
+        Set email function
         
         :param self: self object of class
         :param email: email of object
+        :returns: return None
+        """
+        print('Setter email work...')
+        self.__email = email
+    
+    
+    def get_password(self) -> str:
+        """ 
+        Get password function
+        
+        :param self: self object of class
+        :returns: return str
+        """
+        print('Getter password work...')
+        return f'{self.__password}'
+    
+    
+    def set_password(self, password) -> None:
+        """ 
+        Set password function
+        
+        :param self: self object of class
         :param password: password of object
         :returns: return None
         """
-        print('Setter work...')
-        self.__email = email
+        print('Setter password work...')
         self.__password = password
     
     
-    user_property = property(get_user, set_user)
+    email = property(get_email, set_email)
+    password = property(get_password, set_password)
     
 
 a = User()
 print(a.__dict__)
-a.set_user('example@gmail.com', '1234567')
-print(a.get_user())
+a.set_email('example@gmail.com')
+print(a.get_email())
+a.set_password('1234567')
+print(a.get_password())
+
+a.email = '222example@gmail.com'
+print(a.email)
+a.password = '987654321'
+print(a.password)
