46459e2413b69bf6e6acb56106c09e61ccee471d
Maks2995 <yakuta.max@yandex.by>
2022-11-27 09:42:19 +0300

    corrected task_1, task_3 on lesson_4

--- maks_yakuta/lesson_4/task_3.py
+++ maks_yakuta/lesson_4/task_3.py
@@ -1,7 +1,7 @@
 list_numbers = [2,3,4,2,3,5,6,7,8,5,4,3,3,4,5,6,7,8,9]
-def amount_element(list: list):
+def amount_element(my_list: list):
     my_dict = {}
-    for element in list:
+    for element in my_list:
         my_dict[element] = my_dict.get(element, 0) + 1
 
     return my_dict
