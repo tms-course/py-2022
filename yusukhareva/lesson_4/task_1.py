793491307509f59a7a89e417fa604bd78c17cd3f
yusukhareva <kimhyoyeon1110@gmail.com>
2022-11-28 19:00:39 +0300

    renamed function in task 1 #lesson_4

--- yusukhareva/lesson_4/task_1.py
+++ yusukhareva/lesson_4/task_1.py
@@ -1,9 +1,9 @@
 """Меняем местами ключи и значения словаря
-:param dict_orig: функция, разбирающая входящий словарь
+:param dict_val_and_keys: функция, разбирающая входящий словарь
 :param d_swap: измененный словарь
 """
-def dict_orig(**kwargs):
+def dict_val_and_keys(**kwargs):
     return {v: k for k, v in kwargs.items()}
 
-d_swap = dict_orig(a= "Anna", b = "Boris")
+d_swap = dict_val_and_keys(a= "Anna", b = "Boris")
 print(d_swap)
