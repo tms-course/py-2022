2e8c13493dd57ea5e4a298bfbdc75ba52a177436
NikZaz <slam.blr.46@yandex.ru>
2022-11-29 13:32:59 +0300

    remove old lesson_6, add new lesson_6 with tasks 1-5 and additions for tasks

--- NZhuromskiy/lesson_6/task_4.py
+++ NZhuromskiy/lesson_6/task_4.py
@@ -8,16 +8,16 @@ import csv
 import random
 from random import randint
 
-code_for_number = [29, 33, 44]
-
 with open('./additional_task_3.json', 'r') as file:
     data = json.load(file)
 
-for person in data:
-    person['phone'] = f"+375 ({random.choice(code_for_number)}) {randint(111, 999)}-{randint(11, 99)}-{randint(11, 99)}"
+
+code_for_number = [29, 33, 44]
+generate_phone_number = lambda: f"+375 ({random.choice(code_for_number)}) " \
+                                f"{randint(111, 999)}-{randint(11, 99)}-{randint(11, 99)}"
 
 with open('./additional_task_4.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(['id', 'name', 'age', 'phone'])
     for person in data:
-        writer.writerow([*person.values()])
+        writer.writerow([*person.values(), generate_phone_number()])
