a3c24500233977a2e24720ad6616c01e6befe2c0
VasilionakA <91758346+VasilionakA@users.noreply.github.com>
2022-11-30 17:42:49 +0200

    Update task_1.py
    Fixed task_1

--- vasilionak/lesson_6/task_1.py
+++ vasilionak/lesson_6/task_1.py
@@ -1,3 +1,11 @@
-s = b'r\xc3\xa9sum\xc3\xa9'
-b = s.decode("utf-8")
-print(b)
+"""Декодировать в строку байтовое значение: b'r\xc3\xa9sum\xc3\xa9'.Затем результат преобоазлвать в байтовый вид в кодировке 'Lathin'
+ и затем результат снова деродировать в троку (результаты всех преобоазований выводить на экран)"""
+
+
+data = b'r\xc3\xa9sum\xc3\xa9'
+data_decode_utf = data.decode("utf-8")
+print(data_decode_utf)
+data_encode_latin = data_decode_utf.encode("Latin1")
+print(data_encode_latin)
+data_decode_latin = data_encode_latin.decode("Latin1")
+print(data_decode_latin)
