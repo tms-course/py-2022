bfd82a9af64259305a80c30f41c0ce648ccff1bc
VasilionakA <vasilenok_nastya@mail.ru>
2022-12-04 19:23:29 +0200

    added task_5

---
+++ vasilionak/lesson_6/task_5.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv('file_3.csv')
+
+data.drop('Age', axis=1, inplace= True)
+
+data_transpose = data.set_axis(['Person 1', 'Person 2', 'Person 3', 'Person 4', 'Person 5']).transpose()
+
+data_transpose.to_excel('data.xlsx')
