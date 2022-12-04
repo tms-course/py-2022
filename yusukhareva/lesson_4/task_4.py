f0aa8b1458493982a9907ecdaae49ca2914d59b7
yusukhareva <kimhyoyeon1110@gmail.com>
2022-12-04 16:50:48 +0300

    changed task_4 #lesson_4

--- yusukhareva/lesson_4/task_4.py
+++ yusukhareva/lesson_4/task_4.py
@@ -1,15 +1,9 @@
 import time 
 from datetime import datetime 
-datetime.strftime(datetime.now(),'%Y-%m-%d %H:%M:%S')
 
-n = int(input("Enter the number: "))
-date_time_list = [i for i in range (n)]
-lst=[]
-for i in date_time_list:
-    
+def defered_date():
     time.sleep(1)
-    lst.append(datetime.strftime(datetime.now(),'%Y-%m-%d %H:%M:%S'))
-    i+=1
-
-print (lst)
-    
+    return datetime.strftime(datetime.now(),'%Y-%m-%d %H:%M:%S')
+n = int(input("Enter the number: "))
+date_list = [defered_date() for _ in range(n)]
+print (date_list)
