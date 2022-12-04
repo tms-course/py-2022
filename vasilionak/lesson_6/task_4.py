389b36e97a1f32cb2ec81215b95a557303e10aaf
VasilionakA <91758346+VasilionakA@users.noreply.github.com>
2022-12-04 18:35:29 +0200

    Update task_4.py
    update task 4

--- vasilionak/lesson_6/task_4.py
+++ vasilionak/lesson_6/task_4.py
@@ -6,13 +6,14 @@ from random import randint
 with open('data.json') as f:
     data = json.load(f)
 
-number_phone = [f'375(44){randint(100, 999)}-{randint(10,99)}-{randint(10,99)}' for _ in range(5)]
 
 with open('file_3.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['id', 'Name', 'Age', 'Phone'])
 
     i = 0
+    for phone in data:
+        number_phone = [f'375(44){randint(100, 999)}-{randint(10,99)}-{randint(10,99)}' for _ in range(5)]
     for key, value in data.items():
         writer.writerow((key, value[0], value[1], number_phone[i]))
         i += 1
