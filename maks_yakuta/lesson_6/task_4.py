ab1582108c44f9a61ce89d5d00c6e063ed560e57
Maks2995 <yakuta.max@yandex.by>
2022-12-04 12:13:06 +0300

    corrected task_4

--- maks_yakuta/lesson_6/task_4.py
+++ maks_yakuta/lesson_6/task_4.py
@@ -6,9 +6,10 @@
 import json
 import csv
 from random import randint
+phone_number = lambda: f"{randint(100, 777)}-{randint(10, 99)}-{randint(10, 99)}"
 with open('task_3.json', 'r') as f:
     data = json.load(f)
-    phone_number = lambda: f"{randint(100, 777)}-{randint(10, 99)}-{randint(10, 99)}"
+
 with open('task_4.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['ID', 'Name', 'Age', 'Phone'])
