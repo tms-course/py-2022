48cf88ee1ee1e25cd379469bf882d030ffdb883f
RomanLezhaiko <roman.lezhaiko@gmail.com>
2022-12-05 23:14:42 +0300

    Fix

--- rlezhaiko/lesson_9/task_1.py
+++ rlezhaiko/lesson_9/task_1.py
@@ -6,11 +6,11 @@
 больше 10, количество слов с 2-мя и более буквами "d" не превышает 2, и каждая "{" 
 имеет "}", то строка валидна.
 """
+from string import punctuation
+
 
 class ContentAnalyzer(object):
     """
-    Class ContentAnalyzer
-    
     Creates an instance of a ContentAnalyzer class
     """
     def __init__(self, filepath: str) -> None:
@@ -20,42 +20,58 @@ class ContentAnalyzer(object):
         self.filepath = filepath
     
     
-    def analyze(self):
+    def analyze(self) -> list:
         """ 
-        Analyze function
+        Read file by filepath line by line and send text to staticmethod data_analyze
         
-        Read file by filepath and send text to staticmethod data_analyze
+        :returns: return list of tuples
         """
+        list_of_returns = []
         with open(self.filepath, 'r') as f:
-            data = f.read()
-            returns = self.data_analyze(data)
-            print(returns)
+            for line in f:
+                returns = self.data_analyze(line.rstrip('\n'))
+                list_of_returns.append(returns)
+        return list_of_returns
     
     
     @staticmethod
-    def data_analyze(data: str) -> list:
+    def data_analyze(data: str) -> tuple:
         """ 
-        Data analyze function (staticmethod)
+        Data analyze function. If the total number of words
+        greater than 10, the number of words with 2 or more "d" letters 
+        does not exceed 2, and each "{" has "}", then the string is valid
         
-        :returns: return list of tuples 
+        :returns: return tuple of boolean valid data and substring of data
         """
-        flag_valid, data, sub_str, list_tmp = False, data.strip(), '', []
-        if len(data) > 7:
-            sub_str = data[:7]
+        flag_valid = False
+        sub_str = data[:7]        
+        list_index_opening_brackets, list_index_closing_brackets = [], []
+        for i in range(len(data)):
+            if data[i] == '{':
+                list_index_opening_brackets.append(i)
+            elif data[i] == '}':
+                list_index_closing_brackets.append(i)
+            
+            if data[i] in punctuation:
+                data = data.replace(data[i], ' ')
         
         words = data.split()
         words_with_more_one_d = list(filter(lambda x: x.count('d') == 2, words))
-        if len(words) > 10:
-            if data.count('{') == data.count('}'):
-                if len(words_with_more_one_d) >= 2:
-                    flag_valid = True
         
-        tuple_tmp_1 = (flag_valid,)
-        tuple_tmp_2 = (sub_str,)
-        list_tmp.extend([tuple_tmp_1, tuple_tmp_2])
-        return list_tmp
+        if (len(words) > 10) and (len(words_with_more_one_d) >= 2):                
+            if (len(list_index_opening_brackets) == len(list_index_closing_brackets)) and (len(list_index_closing_brackets) == 0):
+                flag_valid = True
+            elif (len(list_index_opening_brackets) == len(list_index_closing_brackets)):
+                summ = 1
+                for i in range(len(list_index_opening_brackets)):
+                    if list_index_opening_brackets[i] > list_index_closing_brackets[i]:
+                        summ *= 0
+                        break
+                flag_valid = bool(summ)
+        
+        return (flag_valid, sub_str)
 
 
 path = 'data/text.txt'
 ca = ContentAnalyzer(path)
-ca.analyze()
+print(ca.analyze())
--- rlezhaiko/lesson_9/task_4.py
+++ rlezhaiko/lesson_9/task_4.py
@@ -9,36 +9,13 @@ from __future__ import annotations
 
 class Matrix(object):
     """ 
-    Class Matrix
-    
     Creates an instance of a Matrix class
     """
-    def __init__(self, matrix: list = [], rows: int = 0, columns: int = 0) -> None:
+    def __init__(self, matrix: list = []) -> None:
         """
         :param matrix: list of lists of numbers. Defaults to [].
-        :param rows: row count in matrix. Defaults to 0.
-        :param columns: column count in matrix. Defaults to 0.
         """
-        self.__matrix = matrix
-        self.__row = rows
-        self.__column = columns
-        
-    
-    def mul_by_number(self, number: float) -> Matrix:
-        """
-        Multiplication by number function
-        Multiplication all elements of matrix by number
-        
-        :param number: float number for multiplication matrix
-        :returns: return an instance of a Matrix class
-        """
-        list_tmp, matrix_tmp = [], []
-        for row in self.__matrix:
-            for column in row:
-                list_tmp.append(column * number)
-            matrix_tmp.append(list_tmp)
-            list_tmp = []
-        return Matrix(matrix_tmp)
+        self.matrix = matrix
         
     
     def __add__(self, other: Matrix) -> Matrix:
@@ -57,38 +34,44 @@ class Matrix(object):
                     list_tmp.append(self.__matrix[i][j] + other.__matrix[i][j])
                 matrix_tmp.append(list_tmp)
                 list_tmp = []
-            obj_tmp = Matrix()
-            obj_tmp.set_matrix(matrix_tmp)
-            return obj_tmp
+            return Matrix(matrix_tmp)
     
     
-    def __mul__(self, other: Matrix) -> Matrix:
+    def __mul__(self, other: Matrix | int | float) -> Matrix | None:
         """
-        Multiplication matrix by matrix
+        Multiplication matrix by matrix or matrix by number
         
-        :param other: an instance of a Matrix class
-        :returns: return an instance of a Matrix class
+        :param other: an instance of a Matrix class or integer number or float number
+        :returns: return an instance of a Matrix class or None
         """
-        summ, list_tmp, matrix_tmp = 0, [], []     
-        if self.__row != other.__column:
-            print('Матрицы не могут быть перемножены.')        
-        else:
-            for i in range(self.__row):
-                for j in range(other.__column):
-                    for k in range(self.__column):
-                        summ += self.__matrix[i][k] * other.__matrix[k][j]
-                    list_tmp.append(summ)
-                    summ = 0
+        summ, list_tmp, matrix_tmp = 0, [], [] 
+        if type(other) == Matrix:
+            if self.__row != other.__column:
+                print('Матрицы не могут быть перемножены.')        
+            else:
+                for i in range(self.__row):
+                    for j in range(other.__column):
+                        for k in range(self.__column):
+                            summ += self.__matrix[i][k] * other.__matrix[k][j]
+                        list_tmp.append(summ)
+                        summ = 0
+                    matrix_tmp.append(list_tmp)
+                    list_tmp = []
+                return Matrix(matrix_tmp)
+        elif type(other) == int or type(other) == float:
+            for row in self.__matrix:
+                for column in row:
+                    list_tmp.append(column * other)
                 matrix_tmp.append(list_tmp)
                 list_tmp = []
-            obj_tmp = Matrix()
-            obj_tmp.set_matrix(matrix_tmp)
-            return obj_tmp
+            return Matrix(matrix_tmp)
+        else:
+            print('Вы пытаетесь перемножить матрицу не с числом или с другой матрицей!')
     
     
-    def print_matrix(self) -> None:
+    def __str__(self) -> str:
         """
-        Print matrix in human representation
+        :returns: return matrix in string format
         """
         max_length = 0
         for row in self.__matrix:
@@ -104,13 +87,11 @@ class Matrix(object):
                 data += str(column) + indent_by_length_element + standart_indent
             data += '\n\n'
         data = data.strip()
-        print(data)
+        return data
     
     
     def get_matrix(self) -> list:
         """
-        Getter matrix
-        
         :returns: return list of list of numbers
         """ 
         return self.__matrix
@@ -118,8 +99,6 @@ class Matrix(object):
     
     def set_matrix(self, matrix: list) -> None:
         """
-        Setter matrix
-        
         :param matrix: list of lists of numbers
         """
         flag_valid = True
@@ -135,54 +114,47 @@ class Matrix(object):
         
         if flag_valid:      
             self.__matrix = matrix
-            self.set_count_row()
-            self.set_count_column()
+            self.set_row_count()
+            self.set_column_count()
     
     
-    def get_count_row(self) -> int:
+    def get_row_count(self) -> int:
         """
-        Getter count row
-        
-        :returns: return count row in matrix
+        :returns: return row count in matrix
         """  
         return self.__row
     
     
-    def set_count_row(self) -> None: 
-        """
-        Setter count row
-        """
+    def set_row_count(self) -> None: 
         self.__row = len(self.__matrix)
     
     
-    def get_count_column(self) -> int: 
+    def get_column_count(self) -> int: 
         """
-        Getter count column
-        
-        :returns: return count column in matrix
+        :returns: return column count in matrix
         """
         return self.__column
     
     
-    def set_count_column(self) -> None:
-        """
-        Setter count column
-        """
+    def set_column_count(self) -> None:
         flag_valid = True
-        column_count = len(self.__matrix[0])
-        for row in self.__matrix:
-            if len(row) != column_count:
-                print('Вы ввели не матрицу!')
-                flag_valid = False
-                break
+        if self.__matrix == []:
+            flag_valid = False
+        else:
+            column_count = len(self.__matrix[0])
+            for row in self.__matrix:
+                if len(row) != column_count:
+                    print('Вы ввели не матрицу!')
+                    flag_valid = False
+                    break
         
         if flag_valid:
             self.__column = column_count
         
     
     matrix = property(get_matrix, set_matrix)
-    count_row = property(get_count_row, set_count_row)
-    count_column = property(get_count_column, set_count_column)    
+    row_count = property(get_row_count, set_row_count)
+    column_count = property(get_column_count, set_column_count)    
 
 
 matrix_1 = [[1, 2, 3], 
@@ -193,16 +165,14 @@ matrix_2 = [[9, 8, 7],
             [6, 5, 4], 
             [3, 2, 1]]
 
-matrix_obj_1 = Matrix()
-matrix_obj_2 = Matrix()
-matrix_obj_1.set_matrix(matrix_1)
-matrix_obj_2.set_matrix(matrix_2)
+matrix_obj_1 = Matrix(matrix_1)
+matrix_obj_2 = Matrix(matrix_2)
 
-matr_mul_numb = matrix_obj_1.mul_by_number(3.56)
-matr_mul_numb.print_matrix()
+matrix_mul_numb = matrix_obj_1 * 3.56
+print(str(matrix_mul_numb))
 
 matrix_add = matrix_obj_1 + matrix_obj_2
-matrix_add.print_matrix()
+print(str(matrix_add))
 
 matrix_mul = matrix_obj_1 * matrix_obj_2
-matrix_mul.print_matrix()
+print(str(matrix_mul))
