8f342a162e6f028007ec6bea340d8ce0d1311036
Yegor Pikulik <egorka8595@mail.ru>
2022-12-05 18:41:04 +0300

    fixed_2.0

--- Yegor/lesson_5/task_5.py
+++ Yegor/lesson_5/task_5.py
@@ -9,24 +9,18 @@ Task_5, Lesson_5
 -.777    ->   Вы ввели отрицательно дробное число: -0.777
 """
 def analyze_number(numbers: str) -> str:
-    numbers_sign = 0
-    numbers_sign2 = 0
-    if len(numbers) != len(list(filter(lambda f: f.isdigit() or f == '.' or f == '-', numbers))) or (
-            numbers[1::].find('-') != -1):
-        return f'Вы ввели некорректное число: {numbers}'
-    for char in numbers:
-        if char == '.':
-            numbers_sign = 1
-        if char == '-':
-            numbers_sign2 = 1
-    if numbers_sign == 1 and numbers_sign2 == 1:
-        return("Вы ввели отрицательное дробное число")
-    if numbers_sign == 1 and numbers_sign2 != 1:
-        return ("Вы ввели положительное дробное число")
-    if numbers_sign2 == 1:
-        return("Вы ввели отрицательное целое число")
+    if numbers[0] == '-':
+        sign_of_number = 'отрицательное'
     else:
-        return ("Вы ввели положительное целое число")
+        sign_of_number = 'положительное'
+    if len(numbers) != len(list(filter(lambda f: f.isdigit() or f == '.' or f == '-', numbers))) or (numbers[1::].find('-') != -1):
+        return f'Вы ввели некорректное число: {numbers}'
+    for char in numbers[1:]:
+        if char == '.':
+            return f"Вы ввели {sign_of_number} дробное число: {numbers}"
+    return f"Вы ввели {sign_of_number} целое число: {numbers}"
+
+
 while True:
     numbers = input('введите число:')
     print(analyze_number(numbers))
