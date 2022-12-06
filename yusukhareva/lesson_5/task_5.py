c5e440411fe31de6e1a847c6a9ffa397a536d956
yusukhareva <kimhyoyeon1110@gmail.com>
2022-12-06 23:16:36 +0300

    in task 5 edited conditions

--- yusukhareva/lesson_5/task_5.py
+++ yusukhareva/lesson_5/task_5.py
@@ -5,13 +5,13 @@ returns Число целое, либо дробное положительно,
 """
 
 def str_check(num_to_check: str):
-    if num_to_check.isdigit():
+    if num_to_check.isdigit() or (num_to_check.count('.')==1 and num_to_check[0:num_to_check.find('.')].isdigit() and num_to_check[num_to_check.find('.')+1:len(num_to_check)] == ""):
         print ( "Вы ввели положительное целое число: ", num_to_check)
-    elif num_to_check[0] == "-" and num_to_check.count('-') == 1 and num_to_check[num_to_check.find('-')+1:len(num_to_check)].isdigit():
+    elif num_to_check[0] == "-" and (num_to_check[num_to_check.find('-')+1:len(num_to_check)].isdigit() or (num_to_check.count('.')==1 and num_to_check[num_to_check.find('-')+1:num_to_check.find('.')].isdigit() and num_to_check[num_to_check.find('.')+1:len(num_to_check)] == "")) and num_to_check!= "-.":
         print ( "Вы ввели отрицательное целое число: ", num_to_check)
-    elif num_to_check[0] == "-" and num_to_check.count('-') == 1 and num_to_check.count('.') in (0,1) and num_to_check[num_to_check.find('-')+1:num_to_check.find('.')].isdigit() and num_to_check[num_to_check.find('.')+1:len(num_to_check)].isdigit():
+    elif num_to_check[0] == "-" and num_to_check.count('.') == 1 and (num_to_check[num_to_check.find('-')+1:num_to_check.find('.')].isdigit() or num_to_check[num_to_check.find('-')+1:num_to_check.find('.')] == "") and num_to_check[num_to_check.find('.')+1:len(num_to_check)].isdigit() :
         print ("Вы ввели отрицательное дробное число: ", num_to_check)
-    elif  num_to_check.count('.') in (0,1) and num_to_check[0:num_to_check.find('.')].isdigit() and num_to_check[num_to_check.find('.')+1:len(num_to_check)].isdigit():
+    elif  num_to_check.count('.') == 1 and (num_to_check[0:num_to_check.find('.')].isdigit() or num_to_check[0:num_to_check.find('.')] == "") and num_to_check[num_to_check.find('.')+1:len(num_to_check)].isdigit():
         print ("Вы ввели положительное дробное число: ", num_to_check)
     else:
         print ("Вы ввели некорректное число: ", num_to_check)
