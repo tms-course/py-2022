903681494a7c7583e14b0c8d2f1c440d63de6876
Maksim Shik <mshik@comscore.com>
2022-12-08 16:01:36 +0300

    added fix after review

--- mshik/lesson_9/task_3.py
+++ mshik/lesson_9/task_3.py
@@ -3,7 +3,7 @@
 
 Написать метакласс Registory, который будет отслеживать количество всех соданных
 объектов класса Entry, в свою очередь класс Entry не должен содержать ничего,
-связанного с логикой рабоыт метакласса, кроме class Enrtry(metaclass=Registry).
+связанного с логикой работы метакласса, кроме class Enrtry(metaclass=Registry).
 """
 class Registory(type):
     """
@@ -13,8 +13,8 @@ class Registory(type):
         _instances_counter (int): int to count new instances of class Entry
     """
     _instances_counter = 0
-
-    def __call__(cls) -> None:
+        
+    def __call__(cls, *args, **kwargs) -> None:
         """
         Magic metod increases count in _instances_counter, if new object created for class Entry.
         
@@ -26,6 +26,7 @@ class Registory(type):
         class_name = cls.__name__
         if class_name == "Entry":
             Registory._instances_counter += 1
+        return cls.__new__(cls, *args, **kwargs)
 
 
 class Entry(metaclass=Registory):
@@ -33,6 +34,6 @@ class Entry(metaclass=Registory):
 
 
 for _ in range(5):
-    print(Entry()) # None
+    print(Entry()) # <__main__.Entry object at ...>
 
 print(Registory._instances_counter) # 5
