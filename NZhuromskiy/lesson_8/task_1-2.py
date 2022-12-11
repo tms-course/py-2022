16f489c3b067f869b1f3e7d3f5b37c7741d56c2f
NikZaz <slam.blr.46@yandex.ru>
2022-12-11 20:48:17 +0300

    add lesson_8 with tasks 1-3

---
+++ NZhuromskiy/lesson_8/task_1-2.py
@@ -0,0 +1,69 @@
+import time
+
+
+class Auto:
+    def __init__(self, brand, age: int, mark, color: str = None, weight: int = None):
+        self.age = age
+        self.brand = brand
+        self.mark = mark
+        self.color = color
+        self.weight = weight
+
+    def move(self):
+        """Вывод ссобщения Move"""
+        print('move')
+
+    def stop(self):
+        """Вывод сообщения Stop"""
+        print('stop')
+
+    def birthday(self):
+        """Увеличивает атрибут age на 1"""
+        self.age += 1
+
+
+class Truck(Auto):
+    def __init__(self, brand, age: int, max_load, mark, color: str = None, weight: int = None):
+        super().__init__(brand, age, color, mark, weight)
+        self.max_load = max_load
+
+    def move(self):
+        """Выводит Attention перед move"""
+        print('attention')
+        super().move()
+
+    def load(self):
+        """При вызове пауза 1 сек, потом вывод текста load, затем опять пауза 1 сек"""
+        time.sleep(1)
+        print('load')
+        time.sleep(1)
+
+
+class Car(Auto):
+    def __init__(self, brand, age: int, max_speed: float, mark, color: str = None, weight: int = None):
+        super().__init__(brand, age, mark, color, weight)
+        self.max_speed = max_speed
+
+    def move(self):
+        """после надписи move появляется надпись max_speed is <max_speed>"""
+        super().move()
+        print(f'max_speed is {self.max_speed}')
+
+
+mercedes_benz = Truck('mercedes', 2, 3250, 'GLS', 'black', 2490)
+ferrari = Truck('ferrari', 1, 2600, '812', 'white')
+audi = Car('audi', 3, 238.5, 'Q8', 'mate-grey', 2540)
+bmw = Car('bmw', 4, 225, '7-series')
+
+mercedes_benz.move()
+mercedes_benz.stop()
+mercedes_benz.load()
+mercedes_benz.birthday()
+print(mercedes_benz.brand, mercedes_benz.age, mercedes_benz.max_load, mercedes_benz.mark, mercedes_benz.color,
+      mercedes_benz.weight)
+
+audi.move()
+audi.stop()
+audi.birthday()
+print(mercedes_benz.brand, mercedes_benz.age, mercedes_benz.max_load, mercedes_benz.mark, mercedes_benz.color,
+      mercedes_benz.weight)
---
+++ NZhuromskiy/lesson_8/task_3.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        """можно использовать ___str___(в определённых удобнее читается)"""
+        return f"{self.__class__.__name__}({self.x}, {self.y})"
+
+
+class Circle(Point):
+    def __init__(self, center_x: float, center_y: float, radius: float):
+        super().__init__(center_x, center_y)
+        self.radius = radius
+
+    def __sub__(self, deducted):
+        """Будем возвращать Point при равности радиусов 1-й и 2-й окружностей, а если
+        радиусы будут неравны то должна вернуться Circle"""
+        x = self.x - deducted.x
+        y = self.y - deducted.y
+        if self.radius == deducted.radius:
+            return Point(x, y)
+        return Circle(x, y, abs(self.radius - deducted.radius))
+
+    def __repr__(self):
+        """можно использовать ___str___(в определённых удобнее читается)"""
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.radius})"
+
+
+circle_one = Circle(-6, 4, 5)
+circle_two = Circle(-7, 5, 5)
+
+print(circle_one, circle_two, circle_one - circle_two)
