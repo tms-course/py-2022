091eeccaf782225947fd06ea83ccd3fb2867cec8
Aliaksandr Karpovich <k64ay@outlook.com>
2023-01-11 20:08:25 +0300

    #20 - events_app initial commit & class 18  changes

---
+++ k64ay/class_18/mysite/polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(_('date published'))
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, 
+                                 on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
