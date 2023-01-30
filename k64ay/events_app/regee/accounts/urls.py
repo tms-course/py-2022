00d7f4c8ed2f067625f605fa08229dd1055599d5
Denis Koltovich <koltovich1123@gmail.com>
2023-01-30 19:41:47 +0300

    merged

--- k64ay/events_app/regee/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('logout', views.logout_user, name='logout_user'),
-    path('register', views.register_user, name='register'),
-    path('', include('django.contrib.auth.urls')),
-]
