d281d52897d492b5d239b80383c8326eccb4670b
Denis Koltovich <koltovich1123@gmail.com>
2023-02-04 20:46:34 +0300

    removed log_changes_model

--- dkoltovich/lesson_19/Blog/blogs/models.py
+++ dkoltovich/lesson_19/Blog/blogs/models.py
@@ -9,15 +9,3 @@ class Blog(models.Model):
 
     creation_date = models.DateTimeField(auto_now=True)
 
-
-class ModelChangeLogsModel(models.Model):
-    user_id = models.BigIntegerField(null=False, blank=True, db_index=True)
-    table_name = models.CharField(max_length=132, null=False, blank=True)
-    table_row = models.BigIntegerField(null=False, blank=True)
-    data = models.TextField(null=False, blank=True)
-    action = models.CharField(max_length=16, null=False, blank=True)  # saved or deleted
-    timestamp = models.DateTimeField(null=False, blank=True)
-
-    class Meta:
-        app_label = "blog"
-        db_table = "model_change_logs"
