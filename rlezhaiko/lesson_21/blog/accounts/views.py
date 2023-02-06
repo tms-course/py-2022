36cc72bbbb01b2e47245eee11fd2675103c363f8
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-06 23:46:58 +0300

    Add registration, login and logout functionality

---
+++ rlezhaiko/lesson_21/blog/accounts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('blog_list'))
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(
+                username=username, 
+                password=raw_password)
+
+            login(request, user)
+            
+            return redirect('blog_list')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
--- rlezhaiko/lesson_21/blog/blog/urls.py
+++ rlezhaiko/lesson_21/blog/blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
     path('posts/', include('posts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
