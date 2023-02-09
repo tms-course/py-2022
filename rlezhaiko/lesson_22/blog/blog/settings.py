d9914fc5a78ef70dbe73465e5c9c38b6e0ff21be
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-09 20:40:32 +0300

    Start using postgresql

--- rlezhaiko/lesson_22/blog/blog/settings.py
+++ rlezhaiko/lesson_22/blog/blog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
