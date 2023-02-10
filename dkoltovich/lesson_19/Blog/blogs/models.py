d4b9da64c0be7cd0c485f6f7c307275f1af837a3
Denis Koltovich <koltovich1123@gmail.com>
2023-02-10 13:40:30 +0300

    added lesson 23

--- dkoltovich/lesson_19/Blog/blogs/models.py
+++ dkoltovich/lesson_19/Blog/blogs/models.py
@@ -5,7 +5,13 @@ from django.contrib.auth.models import User
 class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, null=False, blank=False)
-    description = models.CharField(max_length=128, null=True, blank=True)
-
+    description = models.CharField(max_length=256, null=True, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['author', ]),
+            models.Index(fields=['title', ]),
+        ]
 
--- dkoltovich/lesson_19/Blog/posts/urls.py
+++ dkoltovich/lesson_19/Blog/posts/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from posts import views
 
 urlpatterns = [
     path('', views.show_feed, name='feed'),
-    path('create/<int:blog_id>', views.create_post, name='create_post'),
+    path('publish/<int:id>/', views.publish_post, name='publish_post'),
+    path('delete/<int:id>/', views.mark_post_as_deleted, name='mark_posts_as_deleted'),
+    path('create/<int:blog_id>/', views.create_post, name='create_post'),
+    path('update/<int:id>/', views.update_post, name='update_post'),
     path('<int:id>/', views.redirect_to_blog, name='redirect_to_blog'),
     path('blogs/', include('blogs.urls'), name='blogs'),
+    path('search/', views.search_post, name='search_posts'),
 ]
