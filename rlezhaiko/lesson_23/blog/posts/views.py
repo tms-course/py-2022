1d967e1f617770a8f61062b86d26d28e049a5268
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-12 00:31:51 +0300

    Fix query params

--- rlezhaiko/lesson_23/blog/posts/views.py
+++ rlezhaiko/lesson_23/blog/posts/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 
 from .models import Post
+from blogs.models import Blog
 
 
 def list_post(request):
     query = ''
     if request.method == 'POST':
         query = request.POST.get('search', '')
-
+    
     posts = Post.objects.filter(status=Post.STATUS_PUBLISHED, title__contains=query)
+    posts_all = Post.objects.filter(status=Post.STATUS_PUBLISHED)
+    blogs_posts = []
+    for post in list(posts_all):
+        if query in post.blog.title:
+            blogs_posts.append(post)
+    
+    blogs_posts.extend(list(posts))
+
     ctx = {'title': 'All posts',
-           'posts': list(posts),}
+           'posts': set(blogs_posts),}
     return render(request, 'post_list.html', ctx)
 
 
