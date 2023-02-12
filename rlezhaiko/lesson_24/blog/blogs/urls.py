ed903c5bbf43dffb66b0e54a9a260d8cfe4f2d15
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-12 13:30:45 +0300

    Add project to lesson 24 and add status_draft to post and blog models

---
+++ rlezhaiko/lesson_24/blog/blogs/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+# blogs
+urlpatterns = [
+    path('', views.list_blog, name='blog_list'),
+    path('<int:id>/', views.get_blog_content, name='blog_content'),
+    path('create/', views.create_blog, name='blog_create'),
+    path('<int:id>/posts', views.create_post, name='post_create'),
+    path('<int:blog_id>/delete/', views.delete_blog, name='blog_delete'),
+]
