27575da7f603b639d7f200edebf2e2310577d579
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-12 23:03:34 +0300

    Add table representation of models in admin panel

--- rlezhaiko/lesson_24/blog/blogs/admin.py
+++ rlezhaiko/lesson_24/blog/blogs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
 from .models import Blog
 
 
-admin.site.register(Blog)
+@admin.register(Blog)
+class AdminBlog(admin.ModelAdmin):
+    list_display = ('id', 'title', 'theme', 'author', 'creation_date', 'status',)
+    search_fields = ('id', 'author__username')
--- rlezhaiko/lesson_24/blog/posts/admin.py
+++ rlezhaiko/lesson_24/blog/posts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class AdminBlog(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'blog', 'creation_date', 'edition_date', 'status',)
+    search_fields = ('id', 'title',)
