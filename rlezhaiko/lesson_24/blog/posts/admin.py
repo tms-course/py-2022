a18d53401116c6852e7e1ec0335be9cf45d5dc55
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-13 23:33:21 +0300

    Add admin action

--- rlezhaiko/lesson_24/blog/posts/admin.py
+++ rlezhaiko/lesson_24/blog/posts/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import Post
 
 
+@admin.action(description="Publish posts")
+def update_status(modeladmin, request, queryset):
+    queryset.update(status=Post.STATUS_PUBLISHED)
+
+
 @admin.register(Post)
 class AdminBlog(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'blog', 'creation_date', 'edition_date', 'status',)
     search_fields = ('title',)
+    actions = [update_status]
