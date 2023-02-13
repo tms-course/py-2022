9942ce1e6efe785169fc4b0068c90e890ae6c6d4
Aliaksandr Karpovich <k64ay@outlook.com>
2023-02-13 22:28:28 +0300

    #27 - in class changes + permission fixes

--- k64ay/todo_infra/todo_project/tasks/serializers.py
+++ k64ay/todo_infra/todo_project/tasks/serializers.py
@@ -8,3 +8,21 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task 
         fields = ['id', 'desc', 'done', 'created_at']
 
+
+# class TaskSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.PrimaryKeyRelatedField(read_only=True)
+#     desc = serializers.CharField()
+#     done = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+
+#     def create(self, validated_data):
+#         return Task.objects.create(**validated_data)
+
+
+#     def update(self, instance, validated_data):
+#         instance.desc = validated_data.get('desc', instance.desc)
+#         instance.done = validated_data.get('done', instance.done)
+#         instance.save()
+
+#         return instance
--- k64ay/todo_infra/todo_project/tasks/views.py
+++ k64ay/todo_infra/todo_project/tasks/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Task
 from .serializers import TaskSerializer
+from .permissions import IsOwnerPermission
 
 
-class TaskViewSet(ModelViewSet):
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 4
+
+
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
+    filter_backends = [
+       DjangoFilterBackend, filters.SearchFilter,
+       filters.OrderingFilter]
+    filterset_fields = ['done', 'desc']
+    search_fields = ['desc']
+    ordering_fields = ['done', 'created_at']
+    pagination_class = StandardResultsSetPagination
+    # permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
       serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        # Your logic should be all here
+        if self.request.method in ('GET', 'POST'):
+            self.permission_classes = [IsAuthenticated, ]
+        elif self.request.method == 'PUT':
+            self.permission_classes = [IsOwnerPermission, ]
+
+        return super(TaskViewSet, self).get_permissions()
