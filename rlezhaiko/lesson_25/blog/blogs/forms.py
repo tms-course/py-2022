b09d718c881139ae30cc260f79d33afdd9d0187d
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-02-14 23:54:09 +0300

    Add validation on forms on field title

--- rlezhaiko/lesson_25/blog/blogs/forms.py
+++ rlezhaiko/lesson_25/blog/blogs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from.models import Blog
 
@@ -13,3 +14,12 @@ class BlogCreationForm(forms.ModelForm):
         super(BlogCreationForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
+    
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) > 30 or not title.istitle():
+            raise ValidationError('Lenght too much or title startswith lowercase')
+        
+        return title
--- rlezhaiko/lesson_25/blog/posts/forms.py
+++ rlezhaiko/lesson_25/blog/posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from.models import Post
 
@@ -13,3 +14,12 @@ class PostCreationForm(forms.ModelForm):
         super(PostCreationForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
+    
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) > 30 or not title.istitle():
+            raise ValidationError('Lenght too much or title startswith lowercase')
+        
+        return title
