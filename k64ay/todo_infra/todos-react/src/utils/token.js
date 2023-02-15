63178a79b977acbecb2bab1744cf47befbe3b071
Denis Koltovich <koltovich1123@gmail.com>
2023-02-15 21:03:00 +0300

    1

--- k64ay/todo_infra/todos-react/src/utils/token.js
+++
@@ -1,5 +0,0 @@
-const TOKEN_KEY = 'token'
-
-export const getToken = () => (localStorage.getItem(TOKEN_KEY));
-export const setToken = (value) => localStorage.setItem(TOKEN_KEY, value);
-export const removeToken = () => { console.log('removing'); localStorage.removeItem(TOKEN_KEY) };
