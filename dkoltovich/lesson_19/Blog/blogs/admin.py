57bbbe1301fd4a65f85bc232feb756eab4046a80
Denis Koltovich <koltovich1123@gmail.com>
2023-02-20 09:54:29 +0300

    some fixes

--- dkoltovich/lesson_19/Blog/blogs/admin.py
+++ dkoltovich/lesson_19/Blog/blogs/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Blog
 
 
-admin.site.register(Blog)
+@admin.register(Blog)
+class AdminBlog(admin.ModelAdmin):
+    list_display = ('title', 'description', 'creation_date', 'status', 'author', )
+    search_fields = ('title', 'author__username', 'description')
+    readonly_fields = ('creation_date',)
+    ordering = ('-creation_date',)
+    list_filter = ['status', 'creation_date', 'author']
+
