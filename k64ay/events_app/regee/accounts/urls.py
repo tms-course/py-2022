e258394fcf8f75700399b99ce79cd2cb2455f2f8
Denis Koltovich <koltovich1123@gmail.com>
2023-02-20 10:42:45 +0300

    resolved conflicts

---
+++ k64ay/events_app/regee/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('logout', views.logout_user, name='logout_user'),
+    path('register', views.register_user, name='register'),
+    path('', include('django.contrib.auth.urls')),
+]
