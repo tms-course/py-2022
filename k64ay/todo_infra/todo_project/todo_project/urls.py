101e8a2c810f756b833c0c82255051044bb90a73
Aliaksandr Karpovich <k64ay@outlook.com>
2023-02-22 23:42:45 +0300

    #30 - in class changes

--- k64ay/todo_infra/todo_project/todo_project/urls.py
+++ k64ay/todo_infra/todo_project/todo_project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
