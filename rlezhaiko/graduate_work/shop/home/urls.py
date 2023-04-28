8136af0ada379e5e78074d6f9f5e43ee0d54c676
RomanLezhaiko <roman.lezhaiko@gmail.com>
2023-04-28 22:36:47 +0300

    Add static pages into home app

--- rlezhaiko/graduate_work/shop/home/urls.py
+++ rlezhaiko/graduate_work/shop/home/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('about_us/', views.get_about_us_page, name='about_us_page'),
     path('transport_services/', views.get_transport_services_page, name='transport_services_page'),
     path('mission_and_values/', views.get_mission_and_values_page, name='mission_and_values_page'),
-    path('delivery/', views.get_delivery_page, name='delivery_page')
+    path('delivery/', views.get_delivery_page, name='delivery_page'),
+    path('equipment_repair/', views.get_equipment_repair_page, name='equipment_repair_page'),
+    path('replacement_and_return_products', views.get_replacement_and_return_products, name='replacement_and_return_products_page'),
+    path('payment/', views.get_payment_page, name='payment_page'),
+    path('contacts/', views.get_contacts_page, name='contacts_page'),
+    path('customer_reviews/', views.get_customer_reviews_page, name='customer_reviews_page')
 ]
--- rlezhaiko/graduate_work/shop/home/views.py
+++ rlezhaiko/graduate_work/shop/home/views.py
@@ -9,6 +9,7 @@ def get_home_page(request):
     category = Category.objects.all()
     products = Product.objects.all()
     ctx = {'title': 'Главная',
+           'shop_name': SHOP_NAME,
            'name_header': 'name header',
            'name_footer': 'name footer',
            'categories': list(category),
@@ -20,20 +21,69 @@ def get_home_page(request):
 
 def get_about_us_page(request):
     count = Product.objects.count()
-    ctx = {
-        'count': count,
-        'shop_name': SHOP_NAME
-    }
+    ctx = {'title': 'О нас',
+           'count': count,
+           'shop_name': SHOP_NAME
+           }
     return render(request, 'about_us.html', ctx)
 
 
 def get_transport_services_page(request):
-    return render(request, 'transport_services.html', {'shop_name': SHOP_NAME})
+    ctx = {'title': 'Транспортные услуги',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'transport_services.html', ctx)
+
+
+
+
+
 
 
 def get_mission_and_values_page(request):
-    return render(request, 'mission_and_values.html', {'shop_name': SHOP_NAME})
+    ctx = {'title': 'Миссия и ценности',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'mission_and_values.html', ctx)
 
 
 def get_delivery_page(request):
-    return render(request, 'delivery.html', {'shop_name': SHOP_NAME})
+    ctx = {'title': 'Доставка',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'delivery.html', ctx)
+
+
+def get_equipment_repair_page(request):
+    ctx = {'title': 'Ремонт техники',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'equipment_repair.html', ctx)
+
+
+def get_replacement_and_return_products(request):
+    ctx = {'title': 'Замена и возврат товара',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'replacement_and_return_products.html', ctx)
+
+
+def get_payment_page(request):
+    ctx = {'title': 'Оплата',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'payment.html', ctx)
+
+
+def get_contacts_page(request):
+    ctx = {'title': 'Контакты',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'contacts.html', ctx)
+
+
+def get_customer_reviews_page(request):
+    ctx = {'title': 'Отзывы',
+           'shop_name': SHOP_NAME
+           }
+    return render(request, 'customer_reviews.html', ctx)
